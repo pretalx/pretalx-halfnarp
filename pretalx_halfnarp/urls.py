9f1e7d5083348395b1505ad3d8cdf0e0be2e737d
Tobias Kunze <r@rixx.de>
2026-08-28 14:24:09 +0200

    Use SLUG_REGEX rather than removed SLUG_CHARS

--- pretalx_halfnarp/urls.py
+++ pretalx_halfnarp/urls.py
@@ -1,27 +1,27 @@
 from django.urls import re_path
 
-from pretalx.event.models.event import SLUG_CHARS
+from pretalx.event.models.event import SLUG_REGEX
 
 from .views import frontend, frontend_preferences_api, organiser
 
 urlpatterns = [
     re_path(
-        f"^(?P<event>[{SLUG_CHARS}]+)/p/halfnarp/$",
+        f"^(?P<event>{SLUG_REGEX})/p/halfnarp/$",
         frontend.FrontendView.as_view(),
         name="frontend",
     ),
     re_path(
-        f"^(?P<event>[{SLUG_CHARS}]+)/p/halfnarp/my-preferences/$",
+        f"^(?P<event>{SLUG_REGEX})/p/halfnarp/my-preferences/$",
         frontend_preferences_api.FrontendMyPreferencesApi.as_view(),
         name="frontend_my_preferences_api",
     ),
     re_path(
-        f"^orga/event/(?P<event>[{SLUG_CHARS}]+)/p/halfnarp/$",
+        rf"^orga/event/(?P<event>{SLUG_REGEX})/p/halfnarp/$",
         organiser.OrganiserView.as_view(),
         name="organiser",
     ),
     re_path(
-        f"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/halfnarp/$",
+        rf"^orga/event/(?P<event>{SLUG_REGEX})/settings/p/halfnarp/$",
         organiser.HalfnarpSettingsView.as_view(),
         name="settings",
     ),
